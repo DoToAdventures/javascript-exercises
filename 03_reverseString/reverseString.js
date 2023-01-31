059a5e9ca4f2ad3e298f22d43a7d61ed7eaff0f7
DoToAdventures <leon.jagusch@gmail.com>
2023-01-31 21:14:29 +0100

    Complete reverseString exercise

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,20 @@
-const reverseString = function() {
+const reverseString = function (inputString) {
+    const stringArray = inputString.split("");
+    let reversedString = stringArray.reverse();
+    reversedString.toString();
+    
+    let joinedReversedString = reversedString.join("");
 
+
+
+    return joinedReversedString;
 };
+
+reverseString('hello');
 
 // Do not edit below this line
 module.exports = reverseString;
+/* Take the string into an array of chars 
+Declare a new variable for the end result of the reversed string
+Write a loop that adds individual characters to variable reversedString, going backwards from stringArray.length();
+*/ 
