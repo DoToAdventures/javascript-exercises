34fd7e7207ba9f48786d0a5132e78217a0758c69
DoToAdventures <leon.jagusch@gmail.com>
2023-02-01 02:01:31 +0100

    Complete sumAll exercise

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,28 @@
-const sumAll = function() {
+const sumAll = function (a, b) {
+    if(((a || b) < 0) || typeof a != 'number' || typeof b != 'number') return 'ERROR';
 
+    let numbersBetween;
+    let sum = 0;
+    let smallerNumber;
+
+    if (a < b) 
+    {
+        smallerNumber = a;
+        numbersBetween = (b - a) + 1;
+    }
+    else
+    {
+        smallerNumber = b;
+        numbersBetween = (a - b) + 1;
+    } 
+
+    for(let i = smallerNumber; i <= numbersBetween; i++)
+    {
+        sum += i;
+    }
+    return sum;
 };
+
 
 // Do not edit below this line
 module.exports = sumAll;
